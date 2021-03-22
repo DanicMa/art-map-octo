ce3313926f15eb42e2b8ba7c1c448a6ec3171452
Matěj Daníček <danicek.matej+gh@gmail.com>
2021-03-22 13:28:55 +0100

    Coastline rendering, color variables

--- R/create_art_map.r
+++ R/create_art_map.r
@@ -6,12 +6,21 @@
 #' @examples create_art_map()
 
 create_art_map <- function() {
-  city <- "Praha"
-  country <- "Czechia"
+  library(osmdata)
+  library(ggplot2)
+  library(sf)
+  
+  city <- "Rostock"
+  country <- "Germany"
   dpi <- 200
+  color_primary <- "white"
+  color_secondary <- "grey"
+  color_river <- "blue"
+  color_sea <- color_river
+  color_coast <- color_sea
   
   # obtain coordinates for ggplot
-  bb <- getbb(paste(city, country, sep=", "))
+  bb <- getbb(paste(city, country, sep=", "))  
 
   # obtain the dataset
   print("Downloading information")
@@ -22,7 +31,8 @@ create_art_map <- function() {
 
   small_streets <- bb %>%
     opq() %>%
-    add_osm_feature(key = "highway", value = c("residential", "living_street", "unclassified", "service", "pedestrian", "footway", "track","path")) %>%
+    add_osm_feature(key = "highway", value = c("residential", "living_street", "unclassified", "service", "pedestrian")) %>%
+    #add_osm_feature(key = "highway", value = c("residential", "living_street", "unclassified", "service", "pedestrian", "footway", "track","path")) %>%
     osmdata_sf()
 
   water <- 
@@ -35,20 +45,26 @@ create_art_map <- function() {
     add_osm_feature(key = 'waterway', value = c("river", "riverbank")) %>%
     osmdata_sf()
 
+  coastline <- opq(bbox = bb) %>% 
+    add_osm_feature(key = 'natural', value = 'coastline') %>% 
+    osmdata_sf()
+
   # create map
   print("Creating map")
   final_map <- ggplot() +
           #ggtitle(city") +
-          geom_sf(data = water$osm_polygons, fill = "blue", colour = NA) +
-          geom_sf(data = waterway$osm_polygons, fill = "blue", colour = NA) +
-          geom_sf(data = streets$osm_lines, inherit.aes = FALSE, color = "white", size = .5, alpha = .8) +
-          geom_sf(data = small_streets$osm_lines, inherit.aes = FALSE, color = "grey", size = .3, alpha = .8) +
+          geom_sf(data = water$osm_polygons, fill = color_river, colour = NA) +
+          geom_sf(data = waterway$osm_polygons, fill = color_river, colour = NA) +
+          geom_sf(data = streets$osm_lines, inherit.aes = FALSE, color = color_primary, size = .5, alpha = .8) +
+          geom_sf(data = small_streets$osm_lines, inherit.aes = FALSE, color = color_secondary, size = .3, alpha = .8) +
+          geom_sf(data = coastline$osm_lines, inherit.aes = FALSE, color = color_coast, size = .2, alpha = .8) +
+          geom_sf(data = coastline$osm_polygons, fill = color_coast, colour = NA) +
           coord_sf(xlim = c(min(bb[1,]), max(bb[1,])), ylim = c(min(bb[2,]), max(bb[2,])), expand = FALSE) +
           theme_void() +
           theme(
           axis.ticks = element_blank(),
-          plot.background = element_rect(fill="black"),
-          plot.margin=unit(c(2.4,2.4,2.4,2.4), "cm")
+          #plot.margin=unit(c(0,2.4,2.4,2.4), "cm"),
+          plot.background = element_rect(fill="black")
           )
 
   print("Saving map")
